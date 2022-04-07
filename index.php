1593ef10b6076fe9bed3135aa2662e501ac99aad
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-04-07 15:07:14 +0000

    index.php

--- index.php
+++ index.php
@@ -11,13 +11,15 @@
 		<!-- Css style sheet -->
 		<link rel="stylesheet"dfghj href="./css/style.css" />
 		<title>Surface Area and Volume of a Cone</title>
-	</head>
+	</headlm
 	<body>
+		<!-- Title, Info and image -->
 		<center><?php 
 			echo '<center><h1>Surface Area and Volume of a Cone</h1></center>';
 			echo '<img src="./images/cone.gif" width="30%"/>';
 			echo "<p><center>Fill out the text fields below and find out the surface area and volume of your cone, don't forget to put units!</center></p>"; 
 		?>
+		<!-- Textfields -->
 		<form method = "post">   
 			Radius of the base: <input type="number" name="Radius" step="0.1">   
 			<br><br>   
@@ -25,11 +27,12 @@
 			<br><br>
 			Unit of Measurement: <input type="text" name="Unit"> <br>   
 			<br><br>
+			<!-- Button -->
 			<input type = "submit" name = "enter" value="Calculate">    
 			<br><br>
+		<!-- Variables and Calculations -->
 		<?php  
 			if(isset($_POST['enter'])) {  
-		    		   
 				$radius = $_POST['Radius'];   
 				$height = $_POST['Height']; 
 				$unit = $_POST['Unit'];
@@ -37,8 +40,8 @@
 				$surfaceArea = round($surfaceArea, 2);
 				$volume = 1/3 * (pi() * ($radius ** 2) * $height);
 				$volume = round($volume, 2);
-				echo "<br><br>The surface area is " . $surfaceArea . $unit;
-				echo "<br><br>The volume is " . $volume . $unit . '²';
+				echo "<br><br>The surface area of the cone is " . $surfaceArea . $unit . '²';
+				echo "<br><br>The volume of the cone is " . $volume . $unit . "³";
 			}
 		?>
 	</body>
